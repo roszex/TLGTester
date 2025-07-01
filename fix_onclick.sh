73bba08e660b3517e53ceca1047b2d549258c02c
Максим <maksim@MacBook-Air-Maksim.local>
2025-07-01 03:49:11 +0300

    Очистка проекта: удалены ненужные файлы и папки

--- fix_onclick.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Убираем onclick из всех страниц 7-13
-for page in {7..13}; do
-    echo "Исправляем страницу $page"
-    sed -i '' 's/onclick="goToNextPage()"//g' webapp/page_${page}/index.html
-done
-
-echo "Все onclick удалены!" 
